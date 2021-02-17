709d4934982fc361b48f219494ac3997f9aceaa3
pharm301 <pharm301@gmail.com>
2021-02-17 13:32:42 +0500

    Исправление замечаний. Версия 2

--- app/src/main/java/com/levin/hw2/FavoriteActivity.kt
+++ app/src/main/java/com/levin/hw2/FavoriteActivity.kt
@@ -3,6 +3,7 @@ package com.levin.hw2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -48,9 +49,18 @@ class FavoriteActivity : AppCompatActivity() {
         }
         setContentView(R.layout.activity_favorite)
         setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         initRecycler()
     }
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                this.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
     private fun initRecycler() {
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
